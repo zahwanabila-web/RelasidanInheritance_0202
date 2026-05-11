4176cd6958ae85f95d2d511a1720f56139239cd6
ZahwaNabila <zahwanabila434@gmail.com>
2026-05-11 18:27:50 +0700

    commit5

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -8,4 +8,17 @@ class pasien {
 public:
     string nama;
     vector<dokter*> daftar_dokter;
+
+    pasien(string pNama) :nama(pNama) {
+        cout << "pasien \"" << nama << "\" ada\n";
+    };
+
+    void tambahDokter(dokter*);
+    void cetakDokter();
+};
+
+class dokter {
+public:
+    string nama;
+    vector<pasien*> daftar_pasien;
 };
