14d3d52d69e803a777dc3471291d1b52cc5727d2
ZahwaNabila <zahwanabila434@gmail.com>
2026-05-11 19:55:18 +0700

    commit17

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -30,4 +30,8 @@ public:
     ~pelajar() {
         cout << "pelajar dihapus\n" << endl;
     }
+
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
+    }
 };
