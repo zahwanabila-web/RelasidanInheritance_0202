ef0ec807a5aa74a87b8882c4d1106211b8c3cf45
ZahwaNabila <zahwanabila434@gmail.com>
2026-05-11 19:33:38 +0700

    commit8

---
+++ jantung.h
@@ -0,0 +1,16 @@
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+class Jantung {
+public:
+    
+    Jantung(){
+        cout << "jantung Ditambah\n";
+    }
+
+    ~Jantung() {
+        cout << "Jantung Dimatikan\n";
+    }
+};
+
+#endif
