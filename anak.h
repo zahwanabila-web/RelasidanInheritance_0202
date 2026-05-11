23fba1f6c424b4c39e5a59a889f81e417bcf3809
ZahwaNabila <zahwanabila434@gmail.com>
2026-05-11 17:41:12 +0700

    commit1

---
+++ anak.h
@@ -0,0 +1,17 @@
+#ifndef ANAK_H
+#define ANAK_H
+
+class anak {
+public:
+
+    string nama;
+
+    anak(string pnama) : nama(pnama) {
+        cout << "Anak \"" << nama << "\" ada \n";
+   }
+
+   ~anak() {
+       cout << "Anak \"" << nama << "\" tidak ada\n";
+   }
+};
+#endif
